b54887cad44fd10254d9b67b244404963ae454b8
danielfelipeq <danielquispe.m@gmail.com>
2022-05-10 00:50:21 -0500

    add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Title from "./Title";
+import Subtitle from "./Subtitle";
+import Button from "./Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title title="Probando React" />
+      <Subtitle text="By: Daniel Quispe" />
+      <Button text="Touch me!" />
     </div>
   );
 }
---
+++ src/Subtitle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Subtitle = ({ text }) => {
+  return <h4>{text}</h4>;
+};
+
+export default Subtitle;
